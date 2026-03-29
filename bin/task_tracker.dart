b90dcfa660009a49081da9157807af8bfb77cd3b
joshua <josuethetika01@gmail.com>
2026-03-29 23:21:02 +0000

    new change

--- bin/task_tracker.dart
+++ bin/task_tracker.dart
@@ -23,8 +23,29 @@ class Task {
   }
 }
 
-class TaskManager {}
+class TaskManager {
+final List<Task> _tasks = [];
+void addTask(String description){
+  final newTask=Task(id: _tasks.length+1, description: description, status: "pending", createAt: DateTime.now().toIso8601String(), updateAt: DateTime.now().toIso8601String());
+}
+
+}
 
 void main(List<String> arguments) {
-  // print('Hello world: ${task_tracker.calculate()}!');
+  TaskManager taskManager=TaskManager();
+
+  if (arguments.isEmpty) {
+    print(
+      "error follow this structure : dart run bin/task_tracker.dart <command> [arguments] ",
+    );
+  }  else if(arguments[0]=="add"){
+    if(arguments.length<2){
+      print("error : please provide a description for the task");
+    }else{
+      final description=arguments[1];
+      taskManager.addTask(description);
+      print("task added successfully");
+    }
+      
+  }
 }
